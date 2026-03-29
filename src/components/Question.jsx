8534528346a8545c4cc9a31fab376e80f4b415be
Sandeesh Croos <sandeesh1070@gmail.com>
2026-03-29 18:14:59 +0530

    Add summary section

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -10,6 +10,16 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
 		isCorrect: null,
 	});
 
+	let timer = 10000;
+
+	if (answer.selectedAnswer) {
+		timer = 1000;
+	}
+
+	if (answer.isCorrect !== null) {
+		timer = 2000;
+	}
+
 	function handleSelectAnswer(answer) {
 		setAnswer({
 			selectedAnswer: answer,
@@ -32,14 +42,18 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
 
 	if (answer.selectedAnswer && answer.isCorrect !== null) {
 		answerState = answer.isCorrect ? 'correct' : 'wrong';
-    } else if (answer.selectedAnswer) {
-        answerState = 'answered';
-    }
-    
+	} else if (answer.selectedAnswer) {
+		answerState = 'answered';
+	}
 
 	return (
 		<div id='question'>
-			<QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+			<QuestionTimer
+				key={timer}
+				timeout={timer}
+				onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+				mode={answerState}
+			/>
 			<h2>{QUESTIONS[questionIndex].text}</h2>
 			<Answers
 				answers={QUESTIONS[questionIndex].answers}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../questions.js';
 import Question from './Question.jsx';
-import quizCompleteImg from '../assets/quiz-complete.png';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
@@ -19,12 +19,7 @@ export default function Quiz() {
 	const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
 	if (quizIsComplete) {
-		return (
-			<div id='summary'>
-				<img src={quizCompleteImg} alt='Trophy Icon' />
-				<h2>Quiz Completed!</h2>
-			</div>
-		);
+		return <Summary userAnswers={userAnswers} />;
 	}
 
 	return (
