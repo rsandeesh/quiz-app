bbfb8904c8e44a3683344a1ca9902efacc844dce
Sandeesh Croos <sandeesh1070@gmail.com>
2026-03-29 17:38:09 +0530

    Setting up with the proper state of the react app

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../questions.js';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
@@ -26,21 +26,16 @@ export default function Quiz() {
 			</div>
 		);
 	}
-	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-	shuffledAnswers.sort(() => Math.random() - 0.5);
 
 	return (
 		<div id='quiz'>
 			<div id='question'>
-				<QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
-				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-				<ul id='answers'>
-					{shuffledAnswers.map((answer) => (
-						<li key={answer} className='answer'>
-							<button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-						</li>
-					))}
-				</ul>
+				<Question
+					key={activeQuestionIndex}
+					questionIndex={activeQuestionIndex}
+					onSelectAnswer={handleSelectAnswer}
+					onSkipAnswer={handleSkipAnswer}
+				/>
 			</div>
 		</div>
 	);
